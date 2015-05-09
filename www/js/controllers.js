b5d714550cf3a78342b5c0f76d2bbf73bd83a141
Matthieu Tanguay-Carel <matthieutc@gmail.com>
2015-05-09 17:13:32 +0200

    knows current card and highlights timeline

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,15 +1,35 @@
 angular.module('starter.controllers', [])
 
 .controller('DashCtrl', function($scope) {
+  var calculateDistance = function(element){
+    var top = 100 - $(element).position().top;
+    return Math.abs(top);
+  };
+
   var scrollAll = function(){
     $('.story-content').scroll(function(){
-      console.log(this + ' is being scrolled');
       var scrollers = $('.story-content');
       for(var i = 0; i < scrollers.length; i++){
         if (scrollers[i] == this) continue;
 
-        scrollers[i].scrollTop = this.scrollTop;
+        //scrollers[i].scrollTop = this.scrollTop;
       }
+
+      // choose current card
+      var cards = $(this).find('.card');
+      var winner = cards[0];
+      for(var i = 0; i < cards.length; i++){
+        if (calculateDistance(cards[i]) < calculateDistance(winner)){
+          winner = cards[i];
+        }
+      }
+      var data = $(winner).data();
+      console.log('current card is: ' + data.tag);
+      var activeCell = $('.' + data.year);
+      activeCell.parent().parent().find('td').removeClass('active');
+      activeCell.addClass('active');
+
+      var allCards = $('[data-tag="' + data.tag + '"]')
     });
   };
   setTimeout(scrollAll, 1000);
